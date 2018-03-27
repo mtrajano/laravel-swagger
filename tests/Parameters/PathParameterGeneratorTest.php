e3ddadd389ce6692430f85e03b806dbb7935e0ec
Mauricio Trajano <mauriciot1993@gmail.com>
2018-03-27 09:33:04 +0000

    fixed pathparam file name and added tests

---
+++ tests/Parameters/PathParameterGeneratorTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Mtrajano\LaravelSwagger\Tests\Parameters;
+
+use Mtrajano\LaravelSwagger\Tests\TestCase;
+use Mtrajano\LaravelSwagger\Parameters\PathParameterGenerator;
+
+class PathParameterGeneratorTest extends TestCase
+{
+    public function testRequiredParameter()
+    {
+        $pathParameters = $this->getPathParameters('/users/{id}');
+
+        $this->assertArraySubset([
+            'in' => 'path',
+            'name' => 'id',
+            'required' => true,
+        ], $pathParameters[0]);
+    }
+
+    public function testOptionalParameter()
+    {
+        $pathParameters = $this->getPathParameters('/users/{id?}');
+
+        $this->assertArraySubset([
+            'required' => false,
+        ], $pathParameters[0]);
+    }
+
+    public function testMultipleParameters()
+    {
+        $pathParameters = $this->getPathParameters('/users/{username}/{id?}');
+
+        $this->assertArraySubset([
+            'name' => 'username',
+            'required' => true,
+        ], $pathParameters[0]);
+
+        $this->assertArraySubset([
+            'name' => 'id',
+            'required' => false,
+        ], $pathParameters[1]);
+    }
+
+    public function testEmptyParameters()
+    {
+        $pathParameters = $this->getPathParameters('/users');
+
+        $this->assertEmpty($pathParameters);
+    }
+
+    private function getPathParameters($uri)
+    {
+        return (new PathParameterGenerator('get', $uri, []))->getParameters();
+    }
+}
