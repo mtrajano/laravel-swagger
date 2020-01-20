d257c3b3a19ddf74a4ecf7f3b06b8da4ad674242
Samuel <samuel.martins@coopersystem.com.br>
2020-01-20 14:24:02 -0300

    refactor: adds action extraction methods to Route class
    Co-authored-by: Odilon Garcez <odilon.garcez.moraes@gmail.com>

--- src/DataObjects/Route.php
+++ src/DataObjects/Route.php
@@ -2,9 +2,15 @@
 
 namespace Mtrajano\LaravelSwagger\DataObjects;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Routing\Route as LaravelRoute;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionMethod;
+use RuntimeException;
 
 class Route
 {
@@ -48,6 +54,18 @@ class Route
         return array_map('strtolower', $this->route->methods());
     }
 
+    /**
+     * Get valid http methods from action.
+     *
+     * @return array
+     */
+    public function validMethods()
+    {
+        return array_filter($this->methods(), function ($route) {
+            return $route !== 'head';
+        });
+    }
+
     protected function formatMiddleware()
     {
         $middleware = $this->route->getAction()['middleware'] ?? [];
@@ -55,5 +73,169 @@ class Route
         return array_map(function ($middleware) {
             return new Middleware($middleware);
         }, Arr::wrap($middleware));
+    }
+
+    /**
+     * Get route name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->route->getName();
+    }
+
+    /**
+     * Get model name from Controller DocBlock.
+     *
+     * @return string|null
+     * @throws ReflectionException
+     */
+    private function getModelNameFromControllerDocs(): ?string
+    {
+        $action = $this->action();
+
+        list($class) = is_string($action) ? Str::parseCallback($action) : [null];
+
+        if (!$class) {
+            return null;
+        }
+
+        $reflection = new ReflectionClass($class);
+
+        $docBlock = $reflection->getDocComment();
+
+        $annotations = $this->getModelAnnotations($docBlock);
+        if (empty($annotations)) {
+            return null;
+        }
+
+        return $annotations[0];
+    }
+
+    /**
+     * Get model searching on route.
+     *
+     * @throws ReflectionException|RuntimeException
+     * @return Model|false
+     */
+    public function getModel()
+    {
+        $modelName = $this->getModelNameFromMethodDocs()
+            ?? $this->getModelNameFromControllerDocs();
+
+        if (!$modelName) {
+            return false;
+        }
+
+        if (!is_subclass_of($modelName, Model::class)) {
+            throw new RuntimeException(
+                'The @model must be an instance of ['.Model::class.']'
+            );
+        }
+
+        return new $modelName;
+    }
+
+    /**
+     * Get action DockBlock.
+     *
+     * @return string
+     * @throws ReflectionException
+     */
+    private function getActionDocBlock()
+    {
+        $action = $this->action();
+
+        $actionInstance = is_string($action) ? $this->getActionClassInstance($action) : null;
+
+        return $actionInstance ? ($actionInstance->getDocComment() ?: '') : '';
+    }
+
+    /**
+     * Get Model name from method DocBlock.
+     *
+     * @return string|null
+     * @throws ReflectionException
+     */
+    private function getModelNameFromMethodDocs(): ?string
+    {
+        $docBlock = $this->getActionDocBlock();
+
+        $annotations = $this->getModelAnnotations($docBlock);
+
+        if (!$annotations) {
+            return null;
+        }
+
+        return reset($annotations);
+    }
+
+    /**
+     * Return a ReflectionMethod instance from current action.
+     *
+     * @param string $action
+     * @return ReflectionMethod
+     * @throws ReflectionException
+     */
+    private function getActionClassInstance(string $action)
+    {
+        list($class, $method) = Str::parseCallback($action);
+
+        return new ReflectionMethod($class, $method);
+    }
+
+    /**
+     * Check the action has a FormRequest on params.
+     *
+     * @return bool
+     * @throws ReflectionException
+     */
+    public function hasFormRequestOnParams()
+    {
+        if (!is_string($this->action())) {
+            return false;
+        }
+
+        $parameters = $this->getActionClassInstance($this->action())->getParameters();
+
+        foreach ($parameters as $parameter) {
+            $reflectionClass = $parameter->getClass();
+            if (!$reflectionClass) {
+                continue;
+            }
+
+            $class = $reflectionClass->getName();
+
+            if (is_subclass_of($class, FormRequest::class)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get exceptions thrown in action.
+     *
+     * @return array
+     * @throws ReflectionException
+     */
+    public function getThrows()
+    {
+        $docBlock = $this->getActionDocBlock();
+
+        return get_annotations($docBlock, '@throws');
+    }
+
+    /**
+     * Get annotations from specific model.
+     *
+     * @param string $docBlock
+     * @return array
+     */
+    private function getModelAnnotations(string $docBlock): array
+    {
+        return get_annotations($docBlock, '@model');
     }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,132 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Str;
+
 if (!function_exists('strip_optional_char')) {
     function strip_optional_char($uri)
     {
         return str_replace('?', '', $uri);
+    }
+}
+
+if (!function_exists('get_annotations')) {
+    /**
+     * Get annotations from text filtering by name.
+     *
+     * @param string $from  The annotation name with "@". E.g.: "@throws"
+     * @param string $annotationName
+     * @return array
+     */
+    function get_annotations(string $from, string $annotationName): array
+    {
+        preg_match_all('#@(.*?)\n#s', $from, $annotations);
+
+        $foundAnnotations = [];
+        foreach (reset($annotations) as $annotation) {
+            if (Str::startsWith($annotation, $annotationName)) {
+                $foundAnnotations[] = trim(Str::replaceFirst($annotationName, '', $annotation));
+            }
+        }
+
+        return $foundAnnotations;
+    }
+}
+
+if (!function_exists('get_all_model_relations')) {
+    /**
+     * Identify all relationships for a given model
+     *
+     * @todo Create unit test fot this method.
+     * @param Model $model Model
+     * @param string $heritage A flag that indicates whether parent and/or child
+     *                         relationships should be included
+     * @return array
+     * @throws ReflectionException
+     */
+    function get_all_model_relations(Model $model = null, $heritage = 'all') {
+        $modelName = get_class($model);
+        $types = ['children' => 'Has', 'parents' => 'Belongs', 'all' => ''];
+        $heritage = in_array($heritage, array_keys($types)) ? $heritage : 'all';
+
+        $reflectionClass = new ReflectionClass($model);
+        $traits = $reflectionClass->getTraits();
+        $traitMethodNames = [];
+        foreach ($traits as $name => $trait) {
+            $traitMethods = $trait->getMethods();
+            foreach ($traitMethods as $traitMethod) {
+                $traitMethodNames[] = $traitMethod->getName();
+            }
+        }
+
+        // Checking the return value actually requires executing the method.
+        // So use this to avoid infinite recursion.
+        $currentMethod = collect(explode('::', __METHOD__))->last();
+        $filter = $types[$heritage];
+        // The method must be public
+        $methods = $reflectionClass->getMethods(ReflectionMethod::IS_PUBLIC);
+
+        $methods = collect($methods)
+            ->filter(
+                function (ReflectionMethod $method) use (
+                    $modelName,
+                    $traitMethodNames,
+                    $currentMethod
+                ) {
+                    $methodName = $method->getName();
+                    /**
+                     * - The method must not originate in a trait
+                     * - It must not be a magic method
+                     * - It must be in the self scope and not inherited
+                     * - It must be in the this scope and not static
+                     * - It must not be an override of this one
+                     */
+                    if (!in_array($methodName, $traitMethodNames)
+                        && strpos($methodName, '__') !== 0
+                        && $method->class === $modelName
+                        && !$method->isStatic()
+                        && $methodName != $currentMethod
+                    ) {
+                        $parameters = (
+                            new ReflectionMethod($modelName, $methodName)
+                        )->getParameters();
+                        // If required parameters exist, this will be false and
+                        // omit this method
+                        return collect($parameters)->filter(
+                            function (ReflectionParameter $parameter) {
+                                // The method must have no required parameters
+                                return !$parameter->isOptional();
+                            }
+                        )->isEmpty();
+                    }
+                    return false;
+                }
+            )
+            ->map(function (ReflectionMethod $method) use ($model, $filter) {
+                $methodName = $method->getName();
+                /** @var Relation|mixed $relation */
+                $relation = $model->$methodName();
+                // Must return a Relation child. This is why we only want to do
+                // this once
+                if (is_subclass_of($relation, Relation::class)) {
+                    $type = (new ReflectionClass($relation))->getShortName();
+                    // If relation is of the desired heritage
+                    if (!$filter || strpos($type, $filter) === 0) {
+                        return [
+                            'method' => $methodName,
+                            'related_model' => $relation->getRelated(),
+                            'relation' => get_class($relation),
+                        ];
+                    }
+                }
+                return null;
+            })
+            // Remove elements reflecting methods that do not have the desired
+            // return type
+            ->filter()
+            ->toArray();
+
+        return $methods;
     }
 }
