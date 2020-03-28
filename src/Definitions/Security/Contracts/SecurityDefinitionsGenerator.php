b12cf1c8b6ceea74360ffdefc86ef4f6da443c9a
Mauricio Trajano <mauriciot1993@gmail.com>
2020-03-28 11:46:37 -0400

    added static analysis check

--- src/Definitions/Security/Contracts/SecurityDefinitionsGenerator.php
+++ src/Definitions/Security/Contracts/SecurityDefinitionsGenerator.php
@@ -8,14 +8,9 @@ use Mtrajano\LaravelSwagger\LaravelSwaggerException;
 interface SecurityDefinitionsGenerator
 {
     /**
-     * @return array
      * @throws LaravelSwaggerException
      */
     public function generate(): array;
 
-    /**
-     * @param Route|null $route
-     * @return array
-     */
     public function generateForRoute(Route $route): array;
 }
